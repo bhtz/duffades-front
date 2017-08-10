267be3feb3accea13af53e849633969f0e8fb284
Thomas Adjadj <ta@ecofip.com>
2017-08-10 18:24:09 +0200

    form

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RouterLink } from '@angular/router';
 
 @Component({
   templateUrl: './home.component.html'
